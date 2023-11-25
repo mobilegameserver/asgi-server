d916b175e901eb200a6140d495b06b022092fc02
MobileGameServer <mobilegameserver@gmail.com>
2023-11-25 21:49:44 +0900

    Update channel.py

--- library/channel.py
+++ library/channel.py
@@ -1,30 +1,23 @@
-import functools
-
-import typing
 import anyio
 from broadcaster import Broadcast
+from conf.conf import Conf
 
 
-broadcast = Broadcast('redis://localhost:6379')
-
-
-async def run_until_first_complete(*args: typing.Tuple[typing.Callable, dict]) -> None:  # type: ignore[type-arg]  # noqa: E501
-    async with anyio.create_task_group() as task_group:
-
-        async def run(func: typing.Callable[[], typing.Coroutine]) -> None:  # type: ignore[type-arg]  # noqa: E501
-            await func()
-            task_group.cancel_scope.cancel()
-
-        for func, kwargs in args:
-            task_group.start_soon(run, functools.partial(func, **kwargs))
+conf = Conf()
+broadcast = Broadcast(conf.redis_url)
 
 
 async def channel(websocket):
     await websocket.accept()
-    await run_until_first_complete(
-        (channel_receiver, {'websocket': websocket}),
-        (channel_sender, {'websocket': websocket}),
-    )
+
+    async with anyio.create_task_group() as task_group:
+        # run until first is complete
+        async def run_channel_receiver() -> None:
+            await channel_receiver(websocket=websocket)
+            task_group.cancel_scope.cancel()
+
+        task_group.start_soon(run_channel_receiver)
+        await channel_sender(websocket)
 
 
 async def channel_receiver(websocket):
